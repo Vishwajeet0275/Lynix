7df4850714b87a823df6908e4dce8c4316c29f4b
Vishwajeet0275 <vishwajeetwankhede123@gmail.com>
2025-03-12 14:47:29 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,7 @@
-#include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println(“Hello, World”);
+    }
 }
