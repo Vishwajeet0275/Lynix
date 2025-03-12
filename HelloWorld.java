21a452f06e955b85fc91949cad53e4bcfd1726bf
tanushree991 <yadavtanushree2@gmail.com>
2025-03-12 14:49:33 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,23 @@
-system.out.println("hello tanushree")
+import java.util.Scanner;
+class Fibonacci
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("enter n");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("--------------------------------------");
+    int f=0;
+    int s=1;
+    System.out.println(f);
+    System.out.println(s);
+    for(int i=2;i<n;i++)
+    {
+
+        int t=f+s;
+        System.out.println(t); 
+        f=s;
+        s=t;   
+    }
+    }
+}
